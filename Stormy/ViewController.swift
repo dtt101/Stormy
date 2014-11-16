623516cfaf4f52f85192187c92a3c50852d5063b
David Thompson <dtt101@gmail.com>
2014-11-16 19:15:40 +0000

    Completed project

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -17,14 +17,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var precipitationLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
+    
     private let apiKey = "07ebf1d8642b580b75f74857de354238"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+        
+    }
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let forecastURL = NSURL(string: "53.91581,-1.09478", relativeToURL: baseURL)
         let sharedSession = NSURLSession.sharedSession()
+        
         let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
             if (error == nil) {
                 let dataObject = NSData(contentsOfURL: location)
@@ -37,10 +48,41 @@ class ViewController: UIViewController {
                     self.humidityLabel.text = "\(currentWeather.humidity)"
                     self.precipitationLabel.text = "\(currentWeather.precipProbability)"
                     self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    // stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
                 })
+            } else {
+                let netWorkIssueController = UIAlertController(title: "Error", message:"Unable to load weather data", preferredStyle: .Alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                netWorkIssueController.addAction(okButton)
+                
+                let cancelButton = UIAlertAction(title: "Cancel", style: .Default, handler: nil)
+                netWorkIssueController.addAction(cancelButton)
+                
+                self.presentViewController(netWorkIssueController, animated: true, completion: nil)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
+                
             }
         })
         downloadTask.resume()
+    }
+    
+    
+    @IBAction func refresh() {
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+        getCurrentWeatherData()
     }
 
     override func didReceiveMemoryWarning() {
